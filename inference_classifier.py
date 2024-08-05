92159f7525949eb8190893baed1a7e5458db21f0
Code-Nethranand <nethranand12345@gmail.com>
2024-08-05 10:22:07 +0530

    bug fix 05 aug

--- inference_classifier.py
+++ inference_classifier.py
@@ -76,7 +76,7 @@ while True:
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
         cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('S', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
